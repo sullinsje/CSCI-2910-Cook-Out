83988fbebf197c6d259409ff3da3dfe0488c2242
sullinsje <113549108+sullinsje@users.noreply.github.com>
2025-05-03 16:37:49 -0400

    Changed bot token and updated lookup
    lookup was pointing to "inventory" when it should be "items"

--- bot.py
+++ bot.py
@@ -36,14 +36,14 @@ async def employees(ctx):
             reply = "Employees:\n" + "\n".join(f"- {name}" for name in names)
             await ctx.send(reply)
           
-      except requests.RequestException as e:
+    except requests.RequestException as e:
             await ctx.send(f"Failed to get employee(s):\n{e}")
 
 @bot.command(name="lookup")
 async def lookup(ctx, *, item_name: str):
     reply = ""
     try:
-        response = requests.get("http://localhost:8000/inventory/", params={"name": item_name})
+        response = requests.get("http://localhost:8000/items/", params={"name": item_name})
         response.raise_for_status()
         items = response.json()
 
@@ -63,4 +63,4 @@ async def lookup(ctx, *, item_name: str):
         await ctx.send(f"Failed to get Item(s):\n{e}")
 
 
-bot.run('MTM2MjI4MDk1MTA5ODUxMTQ3MQ.G9GvLp.J74Ei0h-sn-POPQ0qLVabCl4x_XLzA_uKM4md0')
+bot.run('MTM2MjI4MDk1MTA5ODUxMTQ3MQ.G7wDLv.xoZFP5W-qGsd1igcFMmcYGp-Yli20-hVaeXzks')
