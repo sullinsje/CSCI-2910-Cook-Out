daaa9727775b5c92cbc3814abe60b7d5db72758c
relaxingstew <86404787+relaxingstew@users.noreply.github.com>
2025-05-03 02:34:33 -0400

    Update bot.py
    added !lookup command

--- bot.py
+++ bot.py
@@ -39,4 +39,28 @@ async def employees(ctx):
       except requests.RequestException as e:
             await ctx.send(f"Failed to get employee(s):\n{e}")
 
+@bot.command(name="lookup")
+async def lookup(ctx, *, item_name: str):
+    reply = ""
+    try:
+        response = requests.get("http://localhost:8000/inventory/", params={"name": item_name})
+        response.raise_for_status()
+        items = response.json()
+
+        if not items:
+            await ctx.send(f"No `{item_name}`(s) found")
+            return
+        else:
+            for item in items:
+                name = item.get("name", "Unknown")
+                count = item.get("count", "Unknown")
+                sold = item.get("sold_since_restock", "Unknown")
+                reply += f"- **{name}** | In Stock: `{count}` | Sold Since Restock: `{sold}`\n"
+
+            await ctx.send(reply)
+                
+    except requests.RequestException as e:
+        await ctx.send(f"Failed to get Item(s):\n{e}")
+
+
 bot.run('MTM2MjI4MDk1MTA5ODUxMTQ3MQ.G9GvLp.J74Ei0h-sn-POPQ0qLVabCl4x_XLzA_uKM4md0')
